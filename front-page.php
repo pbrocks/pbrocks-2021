79ccf92c09182a93e0454ca6a42493145c801b49
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-08-14 14:24:20 -0400

    front-page curl

--- front-page.php
+++ front-page.php
@@ -14,6 +14,40 @@ defined( 'ABSPATH' ) || exit;
 
 get_header();
 
+$current_user = wp_get_current_user();
+if ( 'pbrocks' === $current_user->user_login ) {
+	echo '<h3 style="color:salmon;">__FILE__: ' . __FILE__ . '</h3>';
+}
+
+$curl = curl_init();
+
+curl_setopt_array(
+	$curl,
+	array(
+		CURLOPT_URL            => 'https://pbinflocal.us.auth0.com/oauth/token',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING       => '',
+		CURLOPT_MAXREDIRS      => 10,
+		CURLOPT_TIMEOUT        => 30,
+		CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST  => 'POST',
+		CURLOPT_POSTFIELDS     => '{"client_id":"AdM0QIrdzIjpp6vaALfjzDO4oMkzhoaQ","client_secret":"TyWKfecBnAQcprwmDK0Xek9iej6XSAETo8Rb6bOyS_dlAJV4maq-gwZx94b1uGqj","audience":"https://pbinflocal.us.auth0.com/api/v2/","grant_type":"client_credentials"}',
+		CURLOPT_HTTPHEADER     => array(
+			'content-type: application/json',
+		),
+	)
+);
+
+$response = curl_exec( $curl );
+$err      = curl_error( $curl );
+
+curl_close( $curl );
+
+if ( $err ) {
+	echo 'cURL Error #:' . $err;
+} else {
+	echo $response;
+}
 /* Start the Loop */
 while ( have_posts() ) :
 	the_post();
